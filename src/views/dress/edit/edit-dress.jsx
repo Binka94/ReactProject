c2f29ff254bc5754e04eac8ec0b281fa27ed572b
Binka94 <40492702+Binka94@users.noreply.github.com>
2019-03-13 19:23:23 +0200

    Update edit-dress.jsx

--- src/views/dress/edit/edit-dress.jsx
+++ src/views/dress/edit/edit-dress.jsx
@@ -5,14 +5,17 @@ import WithAuthentication from '../../../hocs/with-user-authentication';
 
 import Input from '../../../components/form/input';
 import Textarea from '../../../components/form/textarea';
-//import Select from '../../../components/form/select';
+import Select from '../../../components/form/select';
 import Loading from '../../../components/common/loading';
 
-import './edit-dress.css';
+import { dressModel } from '../../../models/dress-model';
+import validator from '../../../util/validator';
 
 import DressService from '../../../services/dress-service';
 import ImageService from '../../../services/image-service';
 import DestinationService from '../../../services/destination-service';
+
+import './edit-dress.css';
 
 import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
@@ -25,22 +28,18 @@ class EditDress extends Component {
         super(props);
         
         this.state = {
-            dress: {
-                destination: '',
-                dress: '',
-                color: '',
-                model: '',
-                description: '',
-                primaryImageUrl: '',
-            },
+            dress: dressModel.defaultState,
             dressId: '',
+            instances: '',
             oldImages: [],
             newImages: [],
             imagesToDelete: [],
             dressSelectListItems: [],
             destinationSelectListItems: [],
-            isLoading: false,
-            redirect: false,
+            linearLoading: false,
+            circularLoading: false,
+            successRedirect: false,
+            errorRedirect: false,
         };
 
         this.dressService = new DressService();
@@ -56,13 +55,25 @@ class EditDress extends Component {
     }
 
     async componentDidMount() {
-        this.setState({isLoading: true});
+        this.setState({circularLoading: true});
+
+        const dressSelectListItems = await this.dressService.getSelectListItems();
+        const destinationSelectListItems = await this.destinationService.getSelectListItems();
     
-        const { dressId } = this.props.match.params;
+        const dressId = this.props.match.params.dressId;
         const dressModel = await this.dressService.getById(dressId);
+
+        if(dressModel.error) {
+            console.log(dressModel);
+            this.setState({circularLoading: false, errorRedirect: true});
+            return;
+        }
+
         const dressImages = await this.imageService.getDressImagesByDressId(dressId);
         
         const dress = {
+            destination: dressModel.destination,
+            dress: dressModel.dress,
             color: dressModel.color,
             model: dressModel.model,
             description: dressModel.description,
@@ -80,33 +91,46 @@ class EditDress extends Component {
         this.setState({
             dressId,
             dress,
-            isLoading: false,
+            dressSelectListItems,
+            destinationSelectListItems,
             oldImages: dressImages,
+            circularLoading: false,
         });
 
         window.M.updateTextFields();
     }
-    
+
     onChangeHandler(event) {
+        event.preventDefault();
+
         const { dress } = this.state;
-        const {model, value} = event.target;
+        const { model, value } = event.target;
         
         if(dress.hasOwnProperty(model)) {
+            validator.validateInput(event.target, dressModel.validationOptions[model]);
+            
             dress[model] = value;
-            this.setState({ dress });
+            this.setState({dress});
         }
     }
 
     async onSubmitHandler(event) {
         event.preventDefault();
-        this.setState({isLoading: true});
-
+        
+        this.setState({linearLoading: true});
+        
         const {dressId, dress, imagesToDelete, newImages} = this.state;
+        const isFormValid = validator.validateAllInputs(dress, dressModel.validationOptions);
 
+        if(!isFormValid) {
+            console.log("Form is not valid. Please, complete all fields correctly.");
+            this.setState({linearLoading: false});
+            return;
+        }
         
         await this.dressService.update(dressId, dress, imagesToDelete, newImages);
 
-        this.setState({ isLoading: false, redirect: true });
+        this.setState({ linearLoading: false, successRedirect: true });
     }
 
     onDrop(fileItems) {
@@ -117,7 +141,6 @@ class EditDress extends Component {
 
     onDelete(event) {
         event.preventDefault();
-
         const {
             id: imageId, 
             deletehash,
@@ -160,19 +183,33 @@ class EditDress extends Component {
 
     render() {
         const {
-            redirect,
-            isLoading,
+            successRedirect,
+            errorRedirect,
+            linearLoading,
+            circularLoading,
+            destinationSelectListItems,
+            dressSelectListItems,
             oldImages,
             newImages,
             dress: dressModel,
             dressId,
         } = this.state;
 
-        if(redirect && dressId.length > 0) {
+        if(circularLoading) {
+            return <Loading circle={true} />;
+        }
+
+        if(successRedirect) {
             return <Redirect to={`/dress/details/${dressId}`} />;
         }
 
+        if(errorRedirect) {
+            return <Redirect to={`/`} />;
+        }
+
         const {
+            dress,
+            destination,
             color,
             model,
             description,
@@ -180,96 +217,111 @@ class EditDress extends Component {
 
         const requiredImages = oldImages.length === 0;
 
-        const disabled = isLoading;
-
         return (
-            <div className="row container block">    
-            {
-                isLoading 
-                ? 
-                <Loading innerColor="orange lighten-4" outerColor="orange" /> 
-                : null
-            }
-            <h2 className="orange-text text-lighten-1 center">Edit Dress</h2>
-            <form className="col s10 offset-s1" onSubmit={this.onSubmitHandler}>  
-                <div className="row">
-                    <Input 
-                        name="color"
-                        label="Color"
-                        value={color}
-                        type="text" 
-                        className="l6"
-                        disabled={disabled}
-                        onChangeHandler={this.onChangeHandler} />
-                    <Input 
-                        name="model" 
-                        label="Model" 
-                        value={model}
-                        type="text" 
-                        className="l6" 
-                        disabled={disabled}
-                        onChangeHandler={this.onChangeHandler} />
-                </div>
-                <div className="row">
-                    <Textarea 
-                        name="description" 
-                        value={description}
-                        label="Description" 
-                        className="l12"
-                        disabled={disabled}
-                        onChangeHandler={this.onChangeHandler} />
-                </div>
-                <div className="row">
-                    {
-                        oldImages.map((image) => {
-                            const primaryButtonColor = image.isPrimary ? 'green' : 'blue';
-                            const primaryButtonIcon = image.isPrimary ? 'check' : 'image';
+            <div className="row container block">
+                <h2 className="orange-text text-lighten-1 center">Edit Dress</h2>
+                <form className="col s10 offset-s1" onSubmit={this.onSubmitHandler}>  
+                    <div className="row">
+                        <Select 
+                            name="dress" 
+                            value={dress}
+                            className="l6"
+                            options={dressSelectListItems} 
+                            onChangeHandler={this.onChangeHandler} />
 
-                            return(
-                                <div className="col s12 m4 l3" key={image._id}>
-                                    <div className="card">
-                                        <div className="card-image">
-                                            <img src={image.link} alt="Dress" />
-                                            <a
-                                                href="#!"
-                                                data-id={image._id}
-                                                data-deletehash={image.deletehash}
-                                                data-link={image.link}
-                                                onClick={this.onDelete}
-                                                className="btn-floating halfway-fab waves-effect waves-light red lighten-1 delete-btn">
-                                                <i className="material-icons">delete</i>
-                                            </a>
-                                            <a
-                                                href="#!"
-                                                data-id={image._id}
-                                                data-link={image.link}
-                                                onClick={this.makePrimary}
-                                                className={`btn-floating halfway-fab waves-effect waves-light lighten-1 primary-btn ${primaryButtonColor}`}>
-                                                <i className="material-icons">{primaryButtonIcon}</i>
-                                            </a>
+                        <Select 
+                            name="destination" 
+                            value={destination}
+                            options={destinationSelectListItems} 
+                            className="l6"
+                            onChangeHandler={this.onChangeHandler} />
+                    </div>
+                    <div className="row">
+                        <Input 
+                            name="color" 
+                            label="Color"
+                            type="text" 
+                            value={color}
+                            className="l4" 
+                            onChangeHandler={this.onChangeHandler} />         
+
+                        <Input 
+                            name="model" 
+                            label="Model"
+                            type="text" 
+                            value={model}
+                            className="l4" 
+                            onChangeHandler={this.onChangeHandler} />         
+                    </div>
+                    <div className="row">
+                        <Textarea 
+                            name="description" 
+                            label="Description" 
+                            value={description}
+                            className="l12"
+                            onChangeHandler={this.onChangeHandler} />
+                    </div>
+                    <div className="row">
+                        {
+                            oldImages.map((image) => {
+                                const primaryButtonColor = image.isPrimary ? 'green' : 'blue';
+                                const primaryButtonIcon = image.isPrimary ? 'check' : 'image';
+
+                                return(
+                                    <div className="col s12 m4 l3" key={image._id}>
+                                        <div className="card">
+                                            <div className="card-image">
+                                                <img src={image.link} alt="Dress" />
+                                                <a
+                                                    href="#!"
+                                                    data-id={image._id}
+                                                    data-deletehash={image.deletehash}
+                                                    data-link={image.link}
+                                                    onClick={this.onDelete}
+                                                    className="btn-floating halfway-fab waves-effect waves-light red lighten-1 delete-btn">
+                                                    <i className="material-icons">delete</i>
+                                                </a>
+                                                <a
+                                                    href="#!"
+                                                    data-id={image._id}
+                                                    data-link={image.link}
+                                                    onClick={this.makePrimary}
+                                                    className={`btn-floating halfway-fab waves-effect waves-light lighten-1 primary-btn ${primaryButtonColor}`}>
+                                                    <i className="material-icons">{primaryButtonIcon}</i>
+                                                </a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            );
-                        })
+                                );
+                            })
+                        }
+                    </div>
+                    <div className="row">
+                        <FilePond 
+                            ref={ref => this.pond = ref}
+                            files={newImages}
+                            maxFiles="5"
+                            required={requiredImages}
+                            allowMultiple={true} 
+                            dropOnPage={true}
+                            onupdatefiles={this.onDrop} />
+                    </div>
+                    {
+                        linearLoading 
+                        ? 
+                        <Loading innerColor="orange lighten-4" outerColor="orange" /> 
+                        : null
                     }
-                </div>
-                <div className="row">
-                    <FilePond                         
-                        ref={ref => this.pond = ref}
-                        files={newImages}
-                        maxFiles="5"
-                        required={requiredImages}
-                        allowMultiple={true} 
-                        dropOnPage={true}
-                        onupdatefiles={this.onDrop} />
-                </div>
-                <button className="btn waves-effect waves-light orange right" type="submit" name="action">Save changes</button>
-            </form>
+                    <button 
+                        className="btn waves-effect waves-light right orange" 
+                        type="submit" 
+                        name="action">
+                        Save changes
+                    </button>
+                </form>
           </div>
         );
     }
 }
-
 
 export default WithAuthentication(EditDress);
